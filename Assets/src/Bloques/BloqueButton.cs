388dd7b294171272999d0efab3b0a17a4f29dbd6
Aaron <aaron.mn03@gmail.com>
2025-02-18 19:40:14 +0100

    Botones de bloques mejorados y sistema de luces

--- Assets/src/Bloques/BloqueButton.cs
+++ Assets/src/Bloques/BloqueButton.cs
@@ -8,8 +8,6 @@ public class BloqueButton : MonoBehaviour
     private DatosBloque datosBloque;
 
     public void InicializarBloque(DatosBloque datosBloque){
-        SetColor(datosBloque.color);
-        SetText(datosBloque.texto, datosBloque.colorTexto);
         SetName(datosBloque.nombre);
         gameObject.SetActive(false);
         gameObject.AddComponent<Button>().onClick.AddListener(() => OnShowBloqueButtonClicked(datosBloque));
@@ -19,23 +17,6 @@ public class BloqueButton : MonoBehaviour
     {
         this.datosBloque = datosBloque;
         GameObject bloque = Instantiate(datosBloque.prefab, GameObject.Find("AreaTrabajo").transform);
-    }
-    private void SetColor(Color color)
-    {
-        bloqueImagen = GetComponent<Image>();
-        if (bloqueImagen != null)
-        {
-            bloqueImagen.color = color;
-        }
-    }
-    private void SetText(string texto, Color color)
-    {
-        bloqueText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-        if (bloqueText != null)
-        {
-            bloqueText.text = texto;
-            bloqueText.color = color;
-        }
     }
 
     private void SetName(string name){
--- Assets/src/Bloques/DatosBloque.cs
+++ Assets/src/Bloques/DatosBloque.cs
@@ -7,9 +7,6 @@ public class DatosBloque : ScriptableObject
 {
     public int id;
     public string nombre;   
-    public string texto;   
-    public Color colorTexto;   
-    public Color color;            
     public GameObject prefab; 
     public GameObject prefabIcon; 
 }
