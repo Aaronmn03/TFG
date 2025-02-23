c9d64dd718ec2b45d4c927cc6e18ecfa945e3971
Aaron <aaron.mn03@gmail.com>
2025-02-23 23:55:10 +0100

    Sistema de pistas añadido

--- Assets/Tutorial/MessageManager.cs
+++ Assets/Tutorial/MessageManager.cs
@@ -11,11 +11,18 @@ public class MessageManager : MonoBehaviour
     private int currentMessageIndex = 0;
     private Coroutine typingCoroutine;
 
+    private bool tutorialFinalizado;
+
     public void Empezar(DatosTutorial datosTutorial)
     {
+        tutorialFinalizado = false;
+        if(datosTutorial == null){
+            Debug.Log("No has agregado mensajes");
+        }
         this.datosTutorial = datosTutorial;
         boton.onClick.AddListener(ShowNextMessage);
         ShowNextMessage();
+        GameObject.Find("info_Button").GetComponent<Button>().onClick.AddListener(ShowPista);
     }
 
     private void ShowNextMessage()
@@ -23,13 +30,14 @@ public class MessageManager : MonoBehaviour
         if (currentMessageIndex < datosTutorial.mensajesBasicos.Length)
         {
             if (typingCoroutine != null)
-                StopCoroutine(typingCoroutine); // Detener la animación anterior si aún está en curso
+                StopCoroutine(typingCoroutine);
             
             typingCoroutine = StartCoroutine(TypeText(datosTutorial.mensajesBasicos[currentMessageIndex]));
             currentMessageIndex++;
         }
         else
         {
+            tutorialFinalizado = true;
             texto.text = ""; 
             boton.onClick.RemoveListener(ShowNextMessage);
             boton.gameObject.SetActive(false);
@@ -38,11 +46,25 @@ public class MessageManager : MonoBehaviour
 
     private IEnumerator TypeText(string message)
     {
-        texto.text = ""; // Limpiar el texto antes de escribir
+        texto.text = "";
         foreach (char letter in message.ToCharArray())
         {
             texto.text += letter;
-            yield return new WaitForSeconds(0.05f); // Velocidad de escritura
+            yield return new WaitForSeconds(0.05f);
         }
+    }
+
+    public void ShowPista(){
+        if(!tutorialFinalizado){return;}
+        
+        boton.gameObject.SetActive(true);
+        int randomIndex = Random.Range(0, datosTutorial.pistas.Length);
+        string mensaje = datosTutorial.pistas[randomIndex];
+
+        if (typingCoroutine != null)
+            StopCoroutine(typingCoroutine);
+
+        typingCoroutine = StartCoroutine(TypeText(mensaje));
+
     }
 }
