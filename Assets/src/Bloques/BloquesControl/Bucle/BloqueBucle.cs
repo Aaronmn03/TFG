9b14b7d4a7050c120d172e5c5f2be6bfabc23d5f
Aaron <aaron.mn03@gmail.com>
2025-04-03 14:15:38 +0200

    style/Añadido el bloque bucle

---
+++ Assets/src/Bloques/BloquesControl/Bucle/BloqueBucle.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloqueBucle : BloqueControl
+{
+    public override IEnumerator Action() {
+        nivel.IfUsed();
+        if (condicion == null){
+            nivel.Lose("Fallo de sintaxis, falta la condicion en el if");
+            yield break;
+        }
+        Coroutine condCoroutine = StartCoroutine(condicion.Action());
+        coroutines.Add(condCoroutine);
+        yield return condCoroutine;
+        if(condicion.ObtenerResultado()){
+            foreach (Bloque bloque in bloquesDentro) {
+                Coroutine c = StartCoroutine(bloque.Action());
+                coroutines.Add(c);
+                yield return c;
+            }
+        }
+    }
+}
