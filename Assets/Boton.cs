4d0241cfec15cc52094e7be35a99e96b0d90c297
Aaron <aaron.mn03@gmail.com>
2025-04-23 18:37:40 +0200

    style: Animacion al boton añadida

--- Assets/Boton.cs
+++ Assets/Boton.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class Boton : ObtenedorVariable
 {
     [SerializeField] bool hasBeenPressed = false;
+    private Animator anim;
     private void Start() {
         base.Start();
         this.valor = hasBeenPressed;
         if (nivel != null) {
             nivel.ResetEvent += Resetear;
         }
+        anim = transform.GetChild(0).GetComponent<Animator>();
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.transform.gameObject.TryGetComponent<ProgramableObject>(out ProgramableObject programableObject)) {
             hasBeenPressed = true;
+            anim.SetBool("isPress", hasBeenPressed);
             this.valor = hasBeenPressed;
         }
     }
 
     private void Resetear(){
         hasBeenPressed = false;
+        anim.SetBool("isPress", hasBeenPressed);
         this.valor = hasBeenPressed;
     }
 }
