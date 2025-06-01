2a48d3b5c60879f69d77ab922d2dbf0e08798394
Aaron <aaron.mn03@gmail.com>
2025-06-01 20:02:55 +0200

    feature: se ha añadido un boton para desbloquear niveles si estamos en modo desarrollo

--- Assets/src/MenuPrincipal/MenuPrincipalController.cs
+++ Assets/src/MenuPrincipal/MenuPrincipalController.cs
@@ -11,45 +11,67 @@ public class MenuPrincipalController : MonoBehaviour
     private GameObject panelNiveles;
     private GameObject panelAjustes;
     private GameObject panelAyuda;
+
+    private GameObject buttonDesbloquearNiveles;
     void Start()
     {
         panelPrincipal = GameObject.Find("MenuPrincipal");
         panelNiveles = GameObject.Find("Niveles");
         panelAjustes = GameObject.Find("Ajustes");
         panelAyuda = GameObject.Find("Ayuda");
+        buttonDesbloquearNiveles = GameObject.Find("ButtonDesbloquearNiveles");
+        #if DEVELOPMENT_BUILD || UNITY_EDITOR
+            botonDesbloquear.SetActive(true);
+        #else
+            botonDesbloquear.SetActive(false);
+        #endif
         MenuPrincipal();
     }
 
-    public void MenuPrincipal(){
+    public void MenuPrincipal()
+    {
         panelPrincipal.SetActive(true);
         panelNiveles.SetActive(false);
         panelAjustes.SetActive(false);
         panelAyuda.SetActive(false);
     }
 
-    public void Niveles(){
+    public void Niveles()
+    {
         panelPrincipal.SetActive(false);
         panelNiveles.SetActive(true);
         gameObject.GetComponent<MenuNivelesController>().OnEnter();
     }
 
-    public void Ajustes(){
+    public void Ajustes()
+    {
         panelPrincipal.SetActive(false);
         panelAjustes.SetActive(true);
         gameObject.GetComponent<MenuAjustesController>().OnEnter();
     }
 
-    public void Ayuda(){
+    public void Ayuda()
+    {
         panelPrincipal.SetActive(false);
         panelAyuda.SetActive(true);
         gameObject.GetComponent<MenuAjustesController>().OnEnter();
     }
 
-    public void ShutOut(){
-        #if UNITY_EDITOR
+    public void ShutOut()
+    {
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-        #else
+#else
         Application.Quit();
+#endif
+    }
+
+    public void DesbloquearNiveles()
+    {
+        #if DEVELOPMENT_BUILD
+            PlayerPrefs.SetInt("MaxLevel", 10);
+            PlayerPrefs.Save();
         #endif
+
     }
 }
